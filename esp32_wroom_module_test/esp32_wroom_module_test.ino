9f4787e1bf96aec44194f1b1357fbc582ac4e698
PfMartin <martinpfatrisch@gmail.com>
2019-10-25 16:26:47 +0200

    updated

--- esp32_wroom_module_test/esp32_wroom_module_test.ino
+++ esp32_wroom_module_test/esp32_wroom_module_test.ino
@@ -1,19 +1,74 @@
-#include "WiFi.h"
+#include <Dps310.h>
 
-int counter = 0;
+// Dps310 Opject
+Dps310 Dps310PressureSensor = Dps310();
 
-void setup() {
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial);
 
-  Serial.println("Setup done");
+
+  //Call begin to initialize Dps310PressureSensor
+  //The parameter 0x76 is the bus address. The default address is 0x77 and does not need to be given.
+  //Dps310PressureSensor.begin(Wire, 0x76);
+  //Use the commented line below instead of the one above to use the default I2C address.
+  //if you are using the Pressure 3 click Board, you need 0x76
+  Dps310PressureSensor.begin(Wire);
+
+  Serial.println("Init complete!");
 }
 
-void loop() {
-  counter++;
-  
-  Serial.println(counter);
-  delay(200);
+
+
+void loop()
+{
+  float temperature;
+  float pressure;
+  uint8_t oversampling = 7;
+  int16_t ret;
+  Serial.println();
+
+  //lets the Dps310 perform a Single temperature measurement with the last (or standard) configuration
+  //The result will be written to the paramerter temperature
+  //ret = Dps310PressureSensor.measureTempOnce(temperature);
+  //the commented line below does exactly the same as the one above, but you can also config the precision
+  //oversampling can be a value from 0 to 7
+  //the Dps 310 will perform 2^oversampling internal temperature measurements and combine them to one result with higher precision
+  //measurements with higher precision take more time, consult datasheet for more information
+  ret = Dps310PressureSensor.measureTempOnce(temperature, oversampling);
+
+  if (ret != 0)
+  {
+    //Something went wrong.
+    //Look at the library code for more information about return codes
+    Serial.print("FAIL! ret = ");
+    Serial.println(ret);
+  }
+  else
+  {
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" degrees of Celsius");
+  }
+
+  //Pressure measurement behaves like temperature measurement
+  //ret = Dps310PressureSensor.measurePressureOnce(pressure);
+  ret = Dps310PressureSensor.measurePressureOnce(pressure, oversampling);
+  if (ret != 0)
+  {
+    //Something went wrong.
+    //Look at the library code for more information about return codes
+    Serial.print("FAIL! ret = ");
+    Serial.println(ret);
+  }
+  else
+  {
+    Serial.print("Pressure: ");
+    Serial.print(pressure);
+    Serial.println(" Pascal");
+  }
+
+  //Wait some time
+  delay(500);
 }
