18fbe986bebcfaa41da89a27e93e6f6c8fbffbe0
PfMartin <45116859+PfMartin@users.noreply.github.com>
2020-01-28 17:51:43 +0100

    Subscribing works

--- weather_station/Visualization/mqtt_subscribe.py
+++ weather_station/Visualization/mqtt_subscribe.py
@@ -1,6 +1,6 @@
 #https://techtutorialsx.com/2017/04/23/python-subscribing-to-mqtt-topic/
 
-import paho.mqtt.client as mqttClient
+import paho.mqtt.client as mqtt
 import time
 
 def on_connect(client, userdata, flags, rc):
@@ -11,35 +11,30 @@ def on_connect(client, userdata, flags, rc):
     else:
         print("Connection failed")
 
-def on_message(client, userdata, message):
-    print("Message received: " + message.payload)
-
-connected = False
+def on_message(client, userdata, msg):
+    print("Message received: " + msg.payload.decode("utf-8"))
+    print("Message topic: " + msg.topic)
+    print("Message qos: " + msg.qos)
+    print("Message retain flag=" + msg.retain)
 
 broker = "192.168.178.62"
 port = 1883
-#user = ""
-#password = ""
 topic = "weather_stats"
 
-client = mqttClient.Client("Python")
-#client.username_pw_set(Martin, password=Jahnel01)
-client.on_connect = on_connect #attach function to the callback
+print("Creating new instance")
+client = mqtt.Client("P1")
 client.on_message = on_message #attach function to the callback
+client.on_connect = on_connect #attach function to the callback
 
+print("Connecting to broker")
 client.connect(broker, port=port)
 client.loop_start()
 
-while connected != True:
-    time.sleep(0.1)
+client.subscribe(topic, qos=0)
+time.sleep(2)
+client.publish(topic, "Message")
+time.sleep(4)
 
-client.subscribe(topic)
+print("Exiting")
+client.loop_stop()
 
-try:
-    while True:
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("exiting")
-    client.disconnect()
-    client.loop_stop()
