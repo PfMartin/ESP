3900ed276789790780131034e02dbfd76dfecd5b
PfMartin <45116859+PfMartin@users.noreply.github.com>
2020-03-23 17:29:18 +0100

    Program for creating a plot for today's temperature

--- weather_station/Visualization/plot_temperature_today.py
+++ weather_station/Visualization/plot_temperature_today.py
@@ -12,17 +12,14 @@ print(len("2020-03-23 1:50:34.344598"))
 x = []
 y = []
 
-with open('%s_temperature.csv' %today, 'r') as file:
+with open('values.csv', 'r') as file:
   data = csv.reader(file, delimiter=',')
   for row in data:
-    if len(row[1]) == 25:
-      x.append(row[1][-14:-10])
-    elif len(row[1]) == 26:
+    if row[1][:10] == today and row[0] != 'value':
       x.append(row[1][-15:-10])
+      y.append(float(row[0]))
 
-    y.append(float(row[0]))
-
-plt.figure(figsize=(10,9))
+plt.figure(figsize=(10, 10))
 
 plt.plot(x, y)
 plt.title('%s\nTemperature over time outside of the Basislager' %today)
