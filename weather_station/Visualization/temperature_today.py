9c3051c496fd3f4fe9ebcea934ffaec3f24f2e8f
PfMartin <45116859+PfMartin@users.noreply.github.com>
2020-03-23 17:04:39 +0100

    Adjusted the figure size

--- weather_station/Visualization/temperature_today.py
+++ weather_station/Visualization/temperature_today.py
@@ -7,6 +7,7 @@ d = datetime.datetime.today()
 today = str(d)[:10]
 
 print('Date: ' + today)
+print(len("2020-03-23 1:50:34.344598"))
 
 x = []
 y = []
@@ -14,16 +15,19 @@ y = []
 with open('%s_temperature.csv' %today, 'r') as file:
   data = csv.reader(file, delimiter=',')
   for row in data:
-    if len(row[1]) == 18:
-      x.append(row[1][-7:-3])
-    elif len(row[1]) == 19:
-      x.append(row[1][-8:-3])
+    if len(row[1]) == 25:
+      x.append(row[1][-14:-10])
+    elif len(row[1]) == 26:
+      x.append(row[1][-15:-10])
 
     y.append(float(row[0]))
 
-plt.plot(x, y, marker = 'x')
-plt.title('Temperature over time outside of the Basislager')
+plt.figure(figsize=(10,9))
+
+plt.plot(x, y)
+plt.title('%s\nTemperature over time outside of the Basislager' %today)
 plt.xlabel('Time of day')
+plt.xticks(rotation=90)
 plt.ylabel('Temperature in degree Celsius')
 
 plt.show()
