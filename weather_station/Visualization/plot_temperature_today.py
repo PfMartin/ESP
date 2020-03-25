820653732dcf87981ff62a3635b96b07e6db27bf
PfMartin <martinpfatrisch@gmail.com>
2020-03-25 10:26:35 +0100

    New values and modified plot

--- weather_station/Visualization/plot_temperature_today.py
+++ weather_station/Visualization/plot_temperature_today.py
@@ -11,6 +11,7 @@ print('Date: ' + today)
 x = []
 y = []
 
+# Store date from file in the lists for x and y
 with open('values.csv', 'r') as file:
   data = csv.reader(file, delimiter=',')
   for row in data:
@@ -18,18 +19,32 @@ with open('values.csv', 'r') as file:
       x.append(row[1][-15:-10])
       y.append(float(row[0]))
 
-plt.figure(1)
+fig = plt.figure()
 
+# Configuring the axes
+ax = fig.add_subplot(1, 1, 1)
+
+y_major_ticks = np.arange(-20, 40, 1)
+y_minor_ticks = np.arange(-20, 40, 0.2)
+
+ax.set_yticks(y_major_ticks)
+ax.set_yticks(y_minor_ticks, minor=True)
+
+# Configuring the plot
 plt.plot(x, y)
 plt.title('%s\nOutside temperature Basis Lager' %today)
-plt.xlabel('Time of day')
+plt.grid(which='major', axis='y', c='black', linewidth='0.2')
+plt.xlabel('Time of the day')
 plt.xticks(rotation=90)
+plt.setp(ax.get_xticklabels()[::2], visible=False)
 plt.ylabel('Temperature in degree Celsius')
 
+# Adjust the window size
 mng = plt.get_current_fig_manager()
 # For Linux system
 mng.resize(*mng.window.maxsize())
 # For Windows
 # mng.window.state('zoomed')
+
 
 plt.show()
